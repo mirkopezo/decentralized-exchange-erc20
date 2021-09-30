9844a7e15ab36b30f45dec6ca7c9f971e584b005
Mirko Pezo <fireover@tutanota.com>
2021-09-30 17:15:40 +0200

    Create token registry

---
+++ contracts/Dex.sol
@@ -0,0 +1,26 @@
+pragma solidity 0.6.3;
+
+contract Dex {
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokens;
+    bytes32[] public tokenList;
+    address public admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress) onlyAdmin() external {
+        tokens[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, 'Only for admin!');
+        _;
+    }
+}
