f4529dff8da725100fb63b0a708c55da9409604a
mirkopezo <accfireover@tutanota.com>
2021-10-13 22:56:34 +0200

    Header component

---
+++ client/src/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Dropdown from 'Dropdown';
+
+function Header(props) {
+    const { user, tokens, contracts, selectToken } = props;
+    return(
+        <header id="header" className="card">
+            <div className="row">
+                <div className="col-sm-3 flex">
+                    <Dropdown 
+                        items={tokens.map(token => ({
+                            label: token.ticker,
+                            value: token
+                        }))}
+                        activeItem={{
+                            label: user.selectedToken.ticker,
+                            value: user.selectToken
+                        }}
+                        onSelect={selectToken}
+                    />
+                </div>
+                <div className="col-sm-9">
+                    <h1 className="header-title">
+                        <span>Dex - </span>
+                        <span className="contract-address">
+                            <span>Contract address: </span>
+                            <span className="address">
+                                {contracts.dex.options.address}
+                            </span>
+                        </span>
+                    </h1>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
