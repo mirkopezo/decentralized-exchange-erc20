16f80f42e27a888fd8d1999d666559d4ec94829a
mirkopezo <accfireover@tutanota.com>
2021-10-22 13:38:36 +0200

    Update to Metamask

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,35 +1,21 @@
 import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 import Dex from 'contracts/Dex.json';
 import ERC20Abi from 'ERC20Abi.json';
 
-const getWeb3 = () => {
-    return new Promise((resolve, reject) => {
-        window.addEventListener("load", async () => {
-            if (window.ethereum) {
+const getWeb3 = () =>
+    new Promise(async(resolve, reject) => {
+        let provider = await detectEthereumProvider();
+        if(provider) {
+            await provider.request({ method: 'eth_requestAccounts' });
+            try {
                 const web3 = new Web3(window.ethereum);
-                try {
-                    await window.ethereum.enable();
-                    resolve(web3);
-                } catch (error) {
-                    reject(error);
-                }
-            }
-            else if (window.web3) {
-                const web3 = window.web3;
-                console.log("Injected web3 detected.");
                 resolve(web3);
+            } catch(error) {
+                reject(error);
             }
-            else {
-                const provider = new Web3.providers.HttpProvider(
-                    "http://localhost:9545"
-                );
-                const web3 = new Web3(provider);
-                console.log("No web3 instance injected, using Local web3.");
-                resolve(web3);
-            }
-        });
+        } reject('Install Metamask');
     });
-};
 
 const getContracts = async web3 => {
     const networkId = await web3.eth.net.getId();
